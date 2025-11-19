83977a12b5523acb6355ca9dde8db2d5f54b8fa4
RzkyNT <rizkiakhsansetiawan@gmail.com>
2025-11-19 16:48:46 +0700

    Fix

---
+++ zip-ext.bat
@@ -0,0 +1,23 @@
+﻿@echo off
+setlocal
+
+REM Nama folder yang akan di-zip
+set FOLDER=extension
+
+REM Nama file output zip
+set OUTPUT=extension.zip
+
+echo ================================
+echo   Compressing %FOLDER% ...
+echo ================================
+
+REM Hapus zip lama jika ada
+if exist %OUTPUT% del %OUTPUT%
+
+REM Proses zip
+powershell -command "Compress-Archive -Path '%FOLDER%\*' -DestinationPath '%OUTPUT%' -Force"
+
+echo.
+echo Selesai! File zip: %OUTPUT%
+pause
+
